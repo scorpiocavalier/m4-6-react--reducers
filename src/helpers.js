25bc443cf2eacb113cc1648d9cd34ce00b6920ab
Scott Morin <scottanthonymorin@gmail.com>
2020-08-04 07:34:26 -0400

    update workshop

--- src/helpers.js
+++ src/helpers.js
@@ -1,13 +1,13 @@
-export const getRowName = rowIndex => {
+export const getRowName = (rowIndex) => {
   return String.fromCharCode(65 + rowIndex);
 };
 
-export const getRowIndex = rowName => {
+export const getRowIndex = (rowName) => {
   return rowName.charCodeAt(0) - 65;
 };
 
-export const getSeatNum = seatIndex => seatIndex + 1;
-export const getSeatIndex = seatNum => seatNum - 1;
+export const getSeatNum = (seatIndex) => seatIndex + 1;
+export const getSeatIndex = (seatNum) => seatNum - 1;
 
 // export const encodeSeatId = (rowIndex, seatIndex) => {
 //   const rowName = getRowName(rowIndex);
