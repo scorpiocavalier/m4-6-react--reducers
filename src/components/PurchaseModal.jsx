a266762c0814e9223dc07c2cb125e8bd11120deb
Arturo Chen <art.can.fly@gmail.com>
2020-08-28 21:03:12 -0400

    + PurchaseModal styling

--- src/components/PurchaseModal.jsx
+++ src/components/PurchaseModal.jsx
@@ -1,23 +1,45 @@
 import React, { useContext, useState } from 'react'
-import Dialog from '@material-ui/core/Dialog'
 import styled from 'styled-components'
+import Dialog from '@material-ui/core/Dialog'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 import { BookingContext, ACTION } from './BookingContext'
 
-export const PurchaseModal = () => {
+export const PurchaseModal = ({ open, onClose }) => {
   const { state, dispatch } = useContext(BookingContext)
   const [creditCard, setCreditCard] = useState('')
   const [expiration, setExpiration] = useState('')
 
-  console.log('state.selectedSeatId', state.selectedSeatId)
-
-  const handleOpen = state.selectedSeatId !== null
-  const handleClose = () => dispatch({ type: ACTION.CANCEL_BOOKING_PROCESS })
-
   return (
-    <Dialog open={handleOpen} onClose={handleClose}>
+    <Dialog open={open} onClose={onClose}>
       <Wrapper>
         <Title>Purchase ticket</Title>
+        <Message>{`You're purchasing 1 ticket for the price of $${state.price}.`}</Message>
+        <SeatWrapper>
+          <Row>
+            <Header>Row</Header>
+            <Header>Seat</Header>
+            <Header>Price</Header>
+          </Row>
+          {open && (
+            <Row>
+              <Data>{state.selectedSeatId.slice(0, 1)}</Data>
+              <Data>{state.selectedSeatId.slice(2)}</Data>
+              <Data>${state.price}</Data>
+            </Row>
+          )}
+        </SeatWrapper>
+        <PaymentWrapper>
+          <PaymentTitle>Enter payment details</PaymentTitle>
+          <Form>
+            <TextField label="Credit card" variant="outlined" required />
+            <TextField label="Expiration" variant="outlined" required />
+            <Button variant="contained" color="primary">
+              Purchase
+            </Button>
+          </Form>
+        </PaymentWrapper>
       </Wrapper>
     </Dialog>
   )
@@ -28,6 +50,53 @@ const Wrapper = styled.div`
   flex-direction: column;
 `
 
+const SeatWrapper = styled(Wrapper)`
+  align-items: center;
+  margin-bottom: 30px;
+`
+
+const PaymentWrapper = styled(Wrapper)`
+  background: #eeeeee;
+  margin-bottom: 30px;
+`
+
 const Title = styled.span`
   font-size: 24px;
+  font-weight: 600;
+  padding: 30px;
+`
+
+const Message = styled.p`
+  margin: 0 30px 20px 30px;
+`
+
+const Row = styled.div`
+  display: flex;
+  width: 70%;
+  padding: 10px 0;
+  border-bottom: 1px solid gray;
+`
+
+const Header = styled.span`
+  font-size: 16px;
+  font-weight: 600;
+  width: 180px;
+  padding: 5px 20px;
+`
+
+const Data = styled(Header)`
+  font-weight: 500;
+  padding: 5px 20px;
+`
+
+const PaymentTitle = styled(Title)`
+  font-size: 18px;
+  padding-bottom: 0;
+`
+
+const Form = styled.form`
+  display: grid;
+  grid-column-gap: 20px;
+  grid-template-columns: 3fr 1.5fr 2fr;
+  padding: 30px;
 `
--- src/components/seat/Seat.jsx
+++ src/components/seat/Seat.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import styled from 'styled-components'
 import Tippy from '@tippy.js/react'
 import 'tippy.js/dist/tippy.css'
@@ -9,22 +9,27 @@ import { PurchaseModal } from '../PurchaseModal'
 
 export default ({ selectedSeatId, rowName, seatNum, price, isBooked }) => {
   const { dispatch } = useContext(BookingContext)
-
+  const [open, setOpen] = useState(false)
   const info = `Row ${rowName}, Seat ${seatNum} - $${price}`
+
+  const handleOpen = () => {
+    setOpen(true)
+    dispatch({
+      type: ACTION.BEGIN_BOOKING_PROCESS,
+      payload: { selectedSeatId, price },
+    })
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+    dispatch({ type: ACTION.CANCEL_BOOKING_PROCESS })
+  }
 
   return (
     <Tippy content={info}>
-      <Button
-        onClick={() =>
-          dispatch({
-            type: ACTION.BEGIN_BOOKING_PROCESS,
-            payload: { selectedSeatId, price },
-          })
-        }
-        disabled={isBooked}
-      >
+      <Button onClick={handleOpen} disabled={isBooked}>
         <Seat isBooked={isBooked} src={svgSeatAvailable} alt="seat" />
-        <PurchaseModal />
+        <PurchaseModal open={open} onClose={handleClose} />
       </Button>
     </Tippy>
   )
